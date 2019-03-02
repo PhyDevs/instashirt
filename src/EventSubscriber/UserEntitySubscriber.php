30966194674d58b84721f4c5d0a33cafabae2720
phydevs <9onfod71@gmail.com>
2019-03-02 02:50:30 +0100

    backend: add User entity subscriber to encode the password

---
+++ src/EventSubscriber/UserEntitySubscriber.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Doctrine\ORM\Events;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserEntitySubscriber implements EventSubscriber
+{
+
+    private $encoder;
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::onFlush,
+        ];
+    }
+
+    public function onFlush(OnFlushEventArgs $args)
+    {
+        $em = $args->getEntityManager();
+        $uow = $em->getUnitOfWork();
+
+        foreach ($uow->getScheduledEntityInsertions() as $entity) {
+            if (!$entity instanceof UserInterface)
+            {
+                return;
+            }
+
+            /** @var User $entity */
+            $entity->setPassword(
+                $this->encoder->encodePassword($entity, $entity->getPassword())
+            );
+
+            $uow->recomputeSingleEntityChangeSet(
+                $em->getClassMetadata(User::class),
+                $entity
+            );
+        }
+
+    }
+}
